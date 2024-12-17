63808738109658214cb6383bc7a806c837805dce
Prajan53 <sprajan53@gmail.com>
2024-12-17 17:19:48 +0530

    complete commit

--- calc-fe/src/screens/home/index.tsx
+++ calc-fe/src/screens/home/index.tsx
@@ -3,6 +3,7 @@ import { SWATCHES } from "@/constants";
 import { ColorSwatch, Group } from "@mantine/core";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
+import Draggable from "react-draggable";
 
 interface Response {
     expr: string;
@@ -21,14 +22,33 @@ export default function Home(){
     const [ color, setColor ] = useState('rgb(255,255,255)');
     const [reset, setReset] = useState(false);
     const [result, setResult] = useState<GeneratedResult>();
+    const [latexExpression, setLatexExpression] = useState<Array<string>>([]);
+    const [latexPosition, setLatexPosition] = useState({x: 10, y: 200});
     const [dictOfVars, setDictOfVars] = useState({});
 
     useEffect(() => {
         if(reset){
             resetCanvas();
+            setLatexExpression([]);
+            setResult(undefined);
+            setDictOfVars({});
             setReset(false);
         }
     }, [reset]) ;
+
+    useEffect(() => {
+        if(latexExpression.length > 0){
+            setTimeout(() => {
+                window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
+            }, 0);
+        }
+    }, [latexExpression])
+
+    useEffect(() => {
+        if(result){
+            renderLatexToCanvas(result.expression, result.answer)
+        }
+    },[result])
 
     useEffect (() => {
         const canvas = canvasRef.current;
@@ -42,7 +62,35 @@ export default function Home(){
                 ctx.lineWidth = 3; //Brush size
             }
         }
+
+        const script = document.createElement('script');
+        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/MathJax.js?config=TeX-MML-AM_CHTML';
+        script.async = true;
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            window.MathJax.Hub.Config({
+                tex2jax: {inlineMath: [['$', '$'], ['\\(', '\\)']]}
+            })
+        };
+
+        return () => {
+            document.head.removeChild(script);
+        }
     }, []);
+
+    const renderLatexToCanvas = (expression: string, answer: string) => {
+        const latex = `\\(\\LARGE{${expression} = ${answer}}\\)`;
+        setLatexExpression([...latexExpression, latex]);
+
+        const canvas = canvasRef.current;
+        if(canvas){
+            const ctx = canvas.getContext('2d');
+            if(ctx) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            }
+        }
+    };
 
     const sendData = async () => {
         const canvas = canvasRef.current;
@@ -57,9 +105,45 @@ export default function Home(){
             });
 
             const resp = await response.data;
-            console.log("Response: ", resp);
+            resp.data.forEach((data: Response) => {
+                if(data.assign === true){
+                    setDictOfVars({
+                        ...dictOfVars,
+                        [data.expr]: data.result
+                    })
+                }
+            })
+
+            const ctx = canvas.getContext('2d');
+            const imageData = ctx!.getImageData(0, 0, canvas.width, canvas.height);
+            let minX = canvas.width, minY = canvas.height, maxX = 0, maxY = 0;
+
+            for(let y = 0; y < canvas.height; y++){
+                for(let x = 0; x < canvas.width; x++){
+                    const i = (y * canvas.width + x) * 4;
+                    if (imageData.data[i+3] > 0){
+                        minX = Math.min(minX, x);
+                        minY = Math.min(minY, y);
+                        maxX = Math.max(maxX, x);
+                        maxY = Math.max(maxY, y)
+                    }
+                }
+            }
+
+            const centerX = (minX + maxX)/2;
+            const centerY = (minY + maxY)/2;
+
+            setLatexPosition({x: centerX, y: centerY});
+            resp.data.forEach((data: Response) => {
+                setTimeout(() => {
+                    setResult({
+                        expression: data.expr,
+                        answer: data.result
+                    });
+                }, 1000)
+            });
         }
-    }
+    };
 
     const resetCanvas = () => {
         const canvas = canvasRef.current;
@@ -125,7 +209,7 @@ export default function Home(){
                     variant='default'
                     color="black"
                 >
-                    Calculate
+                    Run
                 </Button>
             </div>
             <canvas 
@@ -137,6 +221,13 @@ export default function Home(){
                 onMouseUp={stopDrawing}
                 onMouseMove={draw}
                 />
+            {latexExpression && latexExpression.map((latex, index) => (
+                <Draggable key={index} defaultPosition={latexPosition} onStop={(e, data) => setLatexPosition({ x: data.x, y: data.y })}>
+                    <div className="absolute p-2 text-white rounded shadow-md">
+                        <div className="latex-content">{latex}</div>
+                    </div>
+                </Draggable>
+            ))}
         </>
-    )
+    );
 }
